ed7c8eb55dce33fed1941970a1c2f4684d458af9
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-11-13 19:22:11 +0700

    done

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import { useState, useEffect } from 'react'
 
 import Box from '../components/box/box'
@@ -8,22 +9,31 @@ const index = () => {
 
 	const [ data, setData ] = useState([])
 	const [ pages, setPages ] = useState({
-		currentPages : 1,
-		totalPages : null
+		currentPages : 0,
+		totalPages : 0
 	})
 
-	useEffect(() => {
-		fetch ('https://reqres.in/api/users?page=2')
+	const fetchingData = () => {
+		fetch (`https://reqres.in/api/users?page=${ pages['currentPages'] + 1 }`)
 		.then(result => result.json())
 		.then(parsed => {
-			setData(parsed['data'])
+			const pagesTemp = { ...pages }
+
+			pagesTemp['totalPages'] = parsed['total_pages']
+			pagesTemp['currentPages'] = pagesTemp['currentPages'] + 1
+
+			setData(data.concat(parsed['data']))
+			setPages(pagesTemp)
 		})
+	}
+
+	useEffect(() => {
+		fetchingData()
 	}, [])
 
 	const Container = { 
 		display : 'flex',
 		flexDirection : 'column',
-		gap : '20px'
 	}
 
 	const Title = {
@@ -37,15 +47,29 @@ const index = () => {
 				<h1>Infinite Scroll</h1>
 			</div>
 			<div sx={ Container }>
-				{ data.length >= 1 && data.map((item, index) => {
-					return <Box
-						key={ index }
-						firstName={ item['first_name'] }
-						lastName={ item['last_name'] }
-						avatar={ item['avatar'] }
-						email={ item['email'] }
-					/>
-				}) }
+				{ data.length >= 1 && 
+					<InfiniteScroll
+						dataLength={ data.length }
+						hasMore={ pages['currentPages'] < pages['totalPages'] }
+						next={ fetchingData }
+						loader={ <h3>Loading...</h3> }
+						style={{ 
+							display : 'flex', 
+							flexDirection : 'column',
+							gap : '20px' 
+						}}
+					>
+						{ data.map((item, index) => {
+							return <Box
+								key={ index }
+								firstName={ item['first_name'] }
+								lastName={ item['last_name'] }
+								avatar={ item['avatar'] }
+								email={ item['email'] }
+							/> 
+						}) }
+					</InfiniteScroll>
+				}
 			</div>
 		</div>
 	)
